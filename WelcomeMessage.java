16cc8ffc8cd13cf5a8591b47c71eab84a95439dc
V. Divya Sindhu <sindhu.vd8607@gmail.com>
2026-08-26 13:53:28 +0530

    Welcome message

---
+++ WelcomeMessage.java
@@ -0,0 +1,9 @@
+public class WelcomeMessage {
+
+    public static void main(String[] args) {
+      
+        System.out.println("Welcome to Java Programming!");
+        System.out.println("This program demonstrates compilation and execution.");
+
+    }
+}
